f3d38033e7ca4d63e8d3ec7efed525c4af3308ee
divineoba <divineobafemi55@gmail.com>
2026-08-16 00:52:29 +0100

    Redesign home route to serve HTML file

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
-from flask import Flask
-app=Flask(__name__)
-@app.get('/')
-def home(): return '<h1>Mary Reyes Program</h1><p>Website is online.</p>'
-if __name__=='__main__': app.run(host='0.0.0.0',port=5000)
+from flask import Flask, send_file
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return send_file("mary_reyes_program_redesigned.html")
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
