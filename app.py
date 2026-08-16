e6622b3736c816fe829ff39c0e30e6fbebfd4863
divineoba <divineobafemi55@gmail.com>
2026-08-16 01:02:35 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask, send_file
+from flask import Flask, send_from_directory
 import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "mary_reyes_program_redesigned.html")
-    return send_file(file_path)
+    return send_from_directory(
+        os.path.dirname(os.path.abspath(__file__)),
+        "mary_reyes_program_redesigned.html"
+    )
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
